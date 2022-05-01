c9fad7294228b838d97df86cdec0484b5a2426f7
Ednardo Gomes <ednardo@email.com>
2022-05-01 16:29:49 -0300

    Ajustando os inputs e estilo do componente Company.

--- web/src/pages/Company/index.tsx
+++ web/src/pages/Company/index.tsx
@@ -1,5 +1,50 @@
+import { Link } from "react-router-dom";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+
+import { Container, Content, Fields } from "../Company/style";
+
 export function Company() {
   return (
-    <h1>Company</h1>
+    <Container>
+      <Header />
+
+      <Content>
+        <h1>Cadastro de empresa</h1>
+
+        <Fields>
+          <Input
+            label="Nome"
+            name="name"
+            type="text"
+            value="Alguma coisa"
+            onChange={() => {}}
+          />
+
+          <Input
+            label="CNPJ"
+            name="cnpj"
+            type="text"
+            value="00.000.000/000-12"
+            onChange={() => {}}
+          />
+
+          <Input
+            label="Descrição"
+            name="description"
+            type="textarea"
+            value=""
+            onChange={() => {}}
+          />
+        </Fields>
+
+        <div className="buttons">
+          <button>Cadastrar</button>
+          <Link to="/">
+            <button name="cancel">Cancelar</button>
+          </Link>
+        </div>
+      </Content>
+    </Container>
   );
 }
