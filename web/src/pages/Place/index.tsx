d1fe35d3dfbcf48bad6500137dc6e535c493f60c
Ednardo Gomes <ednardo@email.com>
2022-05-04 02:05:07 -0300

    Implementando a função para recuperar endereço pelo cep.

--- web/src/pages/Place/index.tsx
+++ web/src/pages/Place/index.tsx
@@ -9,18 +9,33 @@ import { Container, Content, Fields } from "../Place/style";
 export function Place() {
   const navigate = useNavigate();
 
-  const [newPlace, setNewPlace] = useState({
+  const [newPlace, setnewPlace] = useState({
     name: "",
     address: "",
   });
 
   async function onSubmit() {
+    const getCep = await fetch(
+      `https://viacep.com.br/ws/${newPlace.address}/json/`
+    );
+
+    const cep = await getCep.json();
+
+    if (cep.erro) {
+      toast("CEP Inválido!", {
+        type: "error",
+        autoClose: 3000,
+      });
+
+      return;
+    }
+
     const request = await fetch("http://localhost:3333/savelocation", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ ...newPlace }),
+      body: JSON.stringify({ address: cep, name: newPlace.name }),
     });
 
     if (request.ok) {
@@ -45,15 +60,15 @@ export function Place() {
             name="name"
             type="text"
             value={newPlace.name}
-            onChange={setNewPlace}
+            onChange={setnewPlace}
           />
 
           <Input
-            label="Endereço"
+            label="CEP"
             name="address"
             type="text"
             value={newPlace.address}
-            onChange={setNewPlace}
+            onChange={setnewPlace}
           />
         </Fields>
 
--- web/src/pages/ShowPlaces/index.tsx
+++ web/src/pages/ShowPlaces/index.tsx
@@ -7,16 +7,33 @@ import { Container, Content } from "../ShowPlaces/style";
 interface ShowPlacesProps {
   id: string;
   name: string;
-  address: string;
+  address: {
+    cep: string;
+    logradouro: string;
+    bairro: string;
+    localidade: string;
+    uf: string;
+  };
 }
 
 export function ShowPlaces() {
   const [places, setPlaces] = useState<ShowPlacesProps[]>([]);
 
+  const hadleData = (data: any) => {
+    const mapper = data.map((item: any) => {
+      return {
+        ...item,
+        address: JSON.parse(item.address),
+      };
+    });
+
+    setPlaces(mapper);
+  };
+
   useEffect(() => {
     fetch("http://localhost:3333/alllocation")
       .then((response) => response.json())
-      .then((data) => setPlaces(data));
+      .then((data) => hadleData(data));
   }, []);
 
   return (
@@ -30,7 +47,11 @@ export function ShowPlaces() {
           <thead>
             <tr>
               <th>Nome</th>
-              <th>Endereço</th>
+              <th>CEP</th>
+              <th>logradouro</th>
+              <th>bairro</th>
+              <th>localidade</th>
+              <th>uf</th>
             </tr>
           </thead>
 
@@ -38,7 +59,11 @@ export function ShowPlaces() {
             {places.map((place) => (
               <tr key={place.id}>
                 <td>{place.name}</td>
-                <td>{place.address}</td>
+                <td>{place.address.cep}</td>
+                <td>{place.address.logradouro}</td>
+                <td>{place.address.bairro}</td>
+                <td>{place.address.localidade}</td>
+                <td>{place.address.uf}</td>
               </tr>
             ))}
           </tbody>
