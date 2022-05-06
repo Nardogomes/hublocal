ceb7a7bf80f6b1bad12dc68ba12ef5cb8af94d37
Ednardo Gomes <ednardo@email.com>
2022-05-06 00:31:29 -0300

    Ajustando dados.

--- backend/src/controllers/ResponsibleController.ts
+++ backend/src/controllers/ResponsibleController.ts
@@ -1,11 +1,27 @@
-import { Request, Response} from "express";
+import { Request, Response } from "express";
 import { getRepository } from "typeorm";
 import { Responsible } from "../entity/Responsible";
 
 export async function save(request: Request, response: Response) {
   const responsibleRepository = getRepository(Responsible);
 
-  const saveResponsible = await responsibleRepository.save(request.body);
+  const { cep, logradouro, bairro, numero, localidade, uf } =
+    request.body.address;
+
+  const address = JSON.stringify({
+    cep,
+    logradouro,
+    bairro,
+    numero,
+    localidade,
+    uf,
+  });
+
+  const saveResponsible = await responsibleRepository.save({
+    address,
+    name: request.body.name,
+    contact: request.body.contact,
+  });
 
   return response.status(200).json(saveResponsible);
 }
